0ece5332831e4027669f00756797376fad1edde2
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-08-03 22:23:05 +0500

    updated with test cases

--- index.ts
+++ index.ts
@@ -1,40 +1,44 @@
-import * as bcrypt from "bcrypt-nodejs";
+import { genSalt, hash, compare } from "bcrypt";
 
-export let stringToHash = (PasswordString: string, round = 10): Promise<string> => {
-    return new Promise((resolve, reject) => {
+const stringToHash = (
+  PasswordString: string,
+  saltRounds = 10
+): Promise<string> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const salt = await genSalt(saltRounds);
+      const hashedPassword = await hash(PasswordString, salt);
+      resolve(hashedPassword);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-        bcrypt.genSalt(round, function (err, salt) {
-            if (err) {
-                reject(err);
-            }
-            bcrypt.hash(PasswordString, salt, function () { }, function (err, hashedPassword: string) {
-                if (err) {
-                    reject(err);
-                }
-                resolve(hashedPassword);
-            });
-        });
-    })
-}
+const verifyHash = (
+  realPassword: string,
+  hashString: string
+): Promise<boolean> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await compare(realPassword, hashString);
+      resolve(result); //return with boolean 'Hash' is matched or not
+    } catch (err) {
+      reject(false); //it means hash is invalid
+    }
+  });
+};
 
-export let varifyHash = (realPassword: string, hashString: string): Promise<boolean> => {
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(realPassword, hashString, function (err, result) {//'result'' will be boolean 
-            if (err) {
-                resolve(false);//it means hash is invalid
-            }
-            resolve(result);//return with boolean 'Hash' is matched or not
-        });
-    })
-}
+const validateHash = (hashString: string): Promise<boolean> => {
+  //true or false in resolve, no reject
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await compare("dummy", hashString);
+      resolve(result); //return with boolean 'Hash' is matched or not
+    } catch (err) {
+      reject(false); //it means hash is invalid
+    }
+  });
+};
 
-export let validateHash = (hashString: string): Promise<boolean> => {//true or false in resolve, no reject
-    return new Promise((resolve, reject) => {
-        bcrypt.compare("dummy", hashString, function (err, result) {//'result'' will be boolean 
-            if (err) {
-                resolve(false);//it means Hash is invalid
-            }
-            resolve(true);//it means hash is either matched or not but it is a valid Hash
-        });
-    })
-}
+export { stringToHash, verifyHash, verifyHash as varifyHash, validateHash };
